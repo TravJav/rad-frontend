1e9856e80e83fff4047fdfe14647651e17f66c9f
Travis <16505078+TravJav@users.noreply.github.com>
2024-01-14 16:28:47 -0500

    updated build container

--- scripts/build_container.sh
+++ scripts/build_container.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
-export AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID # your access key id for your dm user account
-export AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY # your secret access key for your dm user account
-export AWS_DEFAULT_REGION=$AWS_DEFAULT_REGION # your default region for your dm user account
+# Set AWS credentials and default region
+export AWS_ACCESS_KEY_ID="$AWS_ACCESS_KEY_ID"         # Your AWS access key ID
+export AWS_SECRET_ACCESS_KEY="$AWS_SECRET_ACCESS_KEY" # Your AWS secret access key
+export AWS_DEFAULT_REGION="$AWS_DEFAULT_REGION"         # Your AWS default region
+
 # Build backend container
 DOCKER_BUILDKIT=1 docker build -t api-data-science:latest -f ./Dockerfile .
+
+# Check if the Docker build was successful, and the image is available
+if docker image inspect api-data-science:latest &> /dev/null; then
+    echo "Docker build successful. Image api-data-science:latest is available."
+else
+    echo "Docker build failed or image not found. Check the build logs for details."
+    exit 1
+fi
